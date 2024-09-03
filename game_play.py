1068657fe12347c16e2b06c31e62f6015982af0d
RoosCocolien <r.steigenga@gmail.com>
2024-09-03 11:07:34 +0200

    Bug fix: enums

--- game_play.py
+++ game_play.py
@@ -15,11 +15,21 @@ class Option(Enum):
 # Game Setup
 def setup_game(name):
     # Create rooms
-    kitchen = Room("Kitchen", "A dank and dirty room buzzing with flies.", x=0, y=0)
-    ballroom = Room("Ballroom", "A large room with shiny wooden floors; it looks like a nice place to dance.", x=-1, y=0)
-    dining_hall = Room("Dining Hall", "A vast room with a long table where a feast could be held.", x=-1, y=1)
-    library = Room("Library", "A quiet place filled with books", x=0, y=-1)
-    garden = Room("Garden", "A lush garden full of flowers", x=1, y=0)
+    kitchen = Room("Kitchen",
+                   "A dark and dirty room buzzing with flies.",
+                   x=0, y=0)
+    ballroom = Room("Ballroom",
+                    "A large room with shiny wooden floors; it looks like a nice place to dance.",
+                    x=-1, y=0)
+    dining_hall = Room("Dining Hall",
+                       "A vast room with a long table where a feast could be held.",
+                       x=-1, y=1)
+    library = Room("Library",
+                   "A quiet place filled with books",
+                   x=0, y=-1)
+    garden = Room("Garden",
+                  "A lush garden full of flowers",
+                  x=1, y=0)
 
     # List of all rooms
     rooms = [kitchen, ballroom, dining_hall, library, garden]
@@ -34,6 +44,7 @@ def setup_game(name):
 
     # Create a player and start the game in the kitchen
     player_in_the_kitchen = Player(name, kitchen, rooms)
+
     return player_in_the_kitchen
 
 
@@ -43,17 +54,17 @@ def play_game(user):
         print(f"\n\n{user.name}, you are in the {user.current_room}")
         command = int(input("Choose an option:"
                             "\n1: move\n2: pick up"
-                            "\n3: inventory\n4: display map of descovered rooms"
+                            "\n3: inventory\n4: display map of discovered rooms"
                             "\n5: quit\n\nOption: "))
 
-        if command == Option.MOVE:
+        if command is Option.MOVE.value:
             direction = input("Provide direction (left|right|up|down): ")
             user.move(direction)
-        elif command == Option.PICK_UP:
+        elif command == Option.PICK_UP.value:
             items = user.current_room.get_items()
             if len(items):
                 print("The following items are available: ")
-                print("0. exit")
+                print("0. I don't want to pick anything up")
                 for item in items:
                     print(f"1. {item.name}")
                 chosen_item = int(input("Which item would you like to pick up: "))
@@ -63,12 +74,18 @@ def play_game(user):
                     continue
             else:
                 print("There are no items available")
-        elif command == Option.INVENTORY:
+        elif command == Option.INVENTORY.value:
             user.show_inventory()
-        elif command == Option.PRINT_MAP:
+        elif command == Option.PRINT_MAP.value:
             user.display_map()
-        elif command == Option.QUIT:
+        elif command == Option.QUIT.value:
             print("Thanks for playing!")
             break
         else:
             print("Invalid command.")
+
+
+if __name__ == "__main__":
+    name = input("What is your name?: ")
+    player = setup_game(name)
+    play_game(player)
